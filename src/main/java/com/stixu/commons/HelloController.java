23781570c929d846eb8e610bbcfc194b6689df10
甘焕 <ganhuan@b.360.cn>
2016-11-01 16:56:26 +0800

    提交数据

--- src/main/java/com/stixu/commons/HelloController.java
+++ src/main/java/com/stixu/commons/HelloController.java
@@ -5,25 +5,29 @@ import javax.inject.Named;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.stixu.commons.security.domain.Account;
+import com.stixu.commons.security.service.AccountService;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
 	
-	private String message;
+	private AccountService accountService;
 	
 	@Inject
-	public HelloController(@Named("username")String message) {
+	public HelloController(AccountService accountService) {
 		super();
-		this.message = message;
+		this.accountService = accountService;
 	}
 
-	@RequestMapping("/hello")
+	@RequestMapping("/detail/{id}")
 	@ResponseBody
-	public String sayHello() {
-		return this.message;
+	public Account findById(@PathVariable("id") String id) {
+		return this.accountService.findById(id);
 	}
 	
 	@GetMapping("/home")
